3a67f5e9ee89a02c2c3aa3753ab41c6045c6bf8a
LeCOdeXx <iuriroberto123@Gmail.com>
2022-10-11 14:34:50 -0300

    Cores

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 			ChessMath chessMatch = new ChessMath();
+			System.out.println();
 			UI.printBoard(chessMatch.getPieces());
 	}
 
