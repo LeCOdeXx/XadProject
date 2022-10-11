c60daa71823a1759a3a4d1fe4fb2bc8bb8e3ebaa
LeCOdeXx <iuriroberto123@Gmail.com>
2022-10-11 13:59:29 -0300

    Adicionei alguns tratamentos

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -27,9 +27,9 @@ public class ChessMath {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new King(board,  Color.BLACK), new Position(7, 4));
-		board.placePiece(new King(board,  Color.BLACK), new Position(0, 4));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board,  Color.BLACK), new Position(1, 4));
+		board.placePiece(new King(board,  Color.BLACK), new Position(2, 4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(4, 4));
 	}
 
 }
